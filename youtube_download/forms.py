ec3706c21d5accd2aa69b9f945257e0830f42f3e
Boris Kayi <boriskayienzo@gmail.com>
2020-05-18 19:14:31 +0200

    Minor changes

--- youtube_download/forms.py
+++ youtube_download/forms.py
@@ -2,6 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
+
 class SearchForm(FlaskForm):
-    video_url = StringField('Paste the YouTube URL here', validators=[DataRequired()])
-    search = SubmitField('Search')
+    video_url = StringField('Paste the YouTube URL here',
+                            validators=[DataRequired()])
+    search = SubmitField('Get Video')
