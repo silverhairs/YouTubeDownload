6f2afe415284ea339eae6aac0fc87f42afc0c9ec
Boris Kayi <boriskayienzo@gmail.com>
2020-05-18 04:25:15 +0200

    Initial commit

---
+++ youtube_download/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from youtube_download.keys import secret_key
+import os
+
+templates_dir = os.path.abspath('youtube_download/pages/')
+app = Flask(__name__, template_folder=templates_dir)
+app.config['SECRET_KEY'] = secret_key # You add your own secret key
+
+from youtube_download import routes
---
+++ youtube_download/routes.py
@@ -0,0 +1,24 @@
+from youtube_download import app
+from flask import render_template, redirect, url_for, request, session
+from pytube import YouTube
+from youtube_download.forms import SearchForm
+
+
+# -> Index
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    title = 'Welcome'
+    form = SearchForm()
+    if form.validate_on_submit():
+        return redirect(url_for('results', yt_url=form.video_url.data))
+    return render_template('index.html', form=form, title=title)
+
+
+# -> Results
+@app.route('/results', methods=['GET', 'POST'])
+def results():
+    title = 'Result'
+    yt_url = request.args['yt_url']
+    video = YouTube(yt_url)
+    resolutions = [stream.resolution for stream in video.streams.filter(progressive=True, file_extension='mp4')]
+    return render_template('results.html', video=video, title=title, resolutions=resolutions)
