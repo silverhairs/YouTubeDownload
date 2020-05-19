17e135844fa72c625bf757701605bd0c7147d968
Boris Kayi <boriskayienzo@gmail.com>
2020-05-19 21:44:35 +0200

    Show hidden keys file

--- youtube_download/__init__.py
+++ youtube_download/__init__.py
@@ -1,9 +1,9 @@
 from flask import Flask
-from youtube_download.keys import secret_key # This file is hidden
+from youtube_download.keys import secret_key 
 import os
 
 templates_dir = os.path.abspath('youtube_download/pages/')
 app = Flask(__name__, template_folder=templates_dir)
-app.config['SECRET_KEY'] = secret_key # You add your own secret key
+app.config['SECRET_KEY'] = secret_key 
 
 from youtube_download import routes
